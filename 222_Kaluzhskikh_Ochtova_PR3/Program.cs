22a62345122d71dd2dd8c9442e63d152e53a45b1
octova <octovavioletta@gmail.com>
2025-02-17 10:19:15 +0300

    Добавьте файлы проекта.

---
+++ 222_Kaluzhskikh_Ochtova_PR3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _222_Kaluzhskikh_Ochtova_PR3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
