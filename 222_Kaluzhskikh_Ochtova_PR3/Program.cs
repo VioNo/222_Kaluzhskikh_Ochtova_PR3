15f6d5fc5ed75e55dffc59dfb88e297a47a8abd4
wosqh <144683836+wosqw@users.noreply.github.com>
2025-02-17 11:24:56 +0300

    Добавлены и отлажены Буквы.

--- 222_Kaluzhskikh_Ochtova_PR3/Program.cs
+++ 222_Kaluzhskikh_Ochtova_PR3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _222_Kaluzhskikh_Ochtova_PR3
 {
@@ -38,19 +35,41 @@ namespace _222_Kaluzhskikh_Ochtova_PR3
             Console.WriteLine("Welcome to Galaxy News!");
             IterateThroughList();
             Console.ReadKey();
+
+            // Буквы
+
+            char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+            string name = "";
+            int[] a = new int[10];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
         }
+
+        // Буквы
+
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
+
+        // Галактика
 
         private static void IterateThroughList()
         {
             var theGalaxies = new List<Galaxy>
-        {
-            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
-            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
-            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
-        };
+            {
+                new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+                new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+                new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+            };
 
             foreach (Galaxy theGalaxy in theGalaxies)
             {
@@ -93,7 +112,6 @@ namespace _222_Kaluzhskikh_Ochtova_PR3
         public object MyGType { get; set; }
         private enum Type { Spiral, Elliptical, Irregular, Lenticular }
     }
-
-
 }
+
 
